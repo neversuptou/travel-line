e355b1148d6bd2a015f84ffd09e1d398814266da
neversuptou <danilasereda2@yandex.ru>
2023-11-26 17:07:22 +0300

    burger-menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@
 import { NAV_LINKS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './Button'
 
 const Navbar = () => {
@@ -11,6 +11,14 @@ const Navbar = () => {
   const toggleNavbar = () => {
     setisClick(!isClick);
   }
+
+  useEffect(() => {
+    if (isClick) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isClick]);
 
   return (
     <nav className='flexBetween max-container padding-container relative z-30 py-5'>
@@ -33,33 +41,31 @@ const Navbar = () => {
       variant="btn_dark_green"
       />
      </div>
-    
-    <button onClick={toggleNavbar}>
-    {isClick ? (
-      <Image src="/close.svg" alt='close' width={32} height={32} className='inline-block cursor-pointer lg:hidden bg-green-500 rounded-xl' />
-    ) : (
-      <Image 
-    src="/menu.svg"
-    alt='menu'
-    width={32}
-    height={32}
-    className='inline-block cursor-pointer lg:hidden'
-    />
-    )}
-    {isClick && (
-      <div className="fixed right-6  lg:hidden bg-green-500 inline-block cursor-pointer lg:hidden h-[100vh] overflow-hidden" >
-        <div className="flex flex-row px-2 py-3 space-y-1 sm:px-3 gap-5">
-          <ul className="h-full gap-12 lg:flex  ">
-        {NAV_LINKS.map((link) => (
-        <Link href={link.href} key={link.key} className='regular-16 block text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
-          {link.label}
-        </Link>
-      ))}
-          </ul>
+
+    <button className='block lg:hidden transtion' onClick={toggleNavbar}>
+      {isClick? (<Image src="/cross.svg" alt='cross' width={32} height={32} className='inline-block cursor-pointer lg:hidden' />
+      ) : (<Image 
+       src="/menu.svg"
+       alt='menu'
+       width={32}
+       height={32}
+       className='inline-block cursor-pointer lg:hidden'
+       />)}
+       {isClick && (
+        
+        <div className='lg:hidden block absolute top-20 w-full left-0 right-0 bg-white transition h-[100vh]'>
+         <ul className='text-center text-xl p-15 align-center'>
+         {NAV_LINKS.map((link) => (
+           <Link href={link.href} key={link.key} className='underline underline-offset-1 regular-24 text-gray-50 flexCenter cursor-pointer transition-all hover:font-bold my-8 py-8  border-green-500 hover:bg-green-500 hover:rounded-full'>
+             {link.label}
+           </Link>
+         ))}
+        </ul>
+        <span className=' regular-15 text-gray-50 flexCenter cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, aliquam quo. Excepturi voluptatibus possimus atque architecto exercitationem debitis, pariatur vel doloremque asperiores? Expedita nemo perferendis tempora. Consequatur animi exercitationem doloremque.</span>
         </div>
-      </div>
+        
+      )}
       
-    )}
     </button>
     </nav>
   )
