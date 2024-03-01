6da23f428417f4d71a7ee411041539e287a220a6
neversuptou <danilasereda2@yandex.ru>
2024-03-01 07:36:52 +0300

    add get app and start footer

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Button = ({type, title, icon, variant}) => {
+const Button = ({type, title, icon, variant,full}) => {
   return (
     <button 
-    className={`flexCenter gap-3 rounded-full border ${variant}`}
+    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
     type='button'>
         {icon && <Image src={icon} alt={title} width={24} height={24} /> }
-        <label className='bold-16 whitespace-nowrap'>{title}</label>
+        <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
   )
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { FOOTER_LINKS } from '@/constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14'>
+        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+          <Link href="/" className='mb-10' >
+            <Image src="logo.svg" alt="logo" width={74} height={29} />
+          </Link>
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                <ul className='regular-14 flex flex-col gap-4 text-gray-30'>
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+const FooterColumn = ({title, children}) => {
+  return (
+    <div>
+      <h4>{title}</h4>
+      {children}
+    </div>
   )
 }
 
